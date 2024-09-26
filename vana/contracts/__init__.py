252d338fa53c2366a7a9a56afddd40b1511b7c4c
Volod <volod@vana.com>
2024-09-26 15:56:59 -0400

    feat: add moksha addresses for TeePool and DataRegistry contracts

--- vana/contracts/__init__.py
+++ vana/contracts/__init__.py
@@ -2,5 +2,9 @@ contracts = {
     "satori": {
         "TeePool": "0x88790ffF10E952ffc13Be22a442616eAfE081594",
         "DataRegistry": "0xDAAD102189FE8D0FE43c1926b109E94D06bD8a97",
-    }
+    },
+    "moksha": {
+        "TeePool": "0xF084Ca24B4E29Aa843898e0B12c465fAFD089965",
+        "DataRegistry": "0xEA882bb75C54DE9A08bC46b46c396727B4BFe9a5",
+    },
 }
