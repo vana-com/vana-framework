f4a01dc9116f759c9133c81a0f098b5f5d508bd2
Volod <volod@vana.com>
2024-11-30 09:00:11 -0500

    feat: update smart contract addresses

--- vana/contracts/__init__.py
+++ vana/contracts/__init__.py
@@ -1,12 +1,12 @@
 contracts = {
     "vana": {
-        "TeePool": "0xF084Ca24B4E29Aa843898e0B12c465fAFD089965",
-        "DataRegistry": "0xEA882bb75C54DE9A08bC46b46c396727B4BFe9a5",
+        "TeePool": "0x3c92fD91639b41f13338CE62f19131e7d19eaa0D",
+        "DataRegistry": "0x8C8788f98385F6ba1adD4234e551ABba0f82Cb7C",
         "RootNetworkContract": "0xf408A064d640b620219F510963646Ed2bD5606BB",
     },
     "islander": {
-        "TeePool": "0xF084Ca24B4E29Aa843898e0B12c465fAFD089965",
-        "DataRegistry": "0xEA882bb75C54DE9A08bC46b46c396727B4BFe9a5",
+        "TeePool": "0x3c92fD91639b41f13338CE62f19131e7d19eaa0D",
+        "DataRegistry": "0x8C8788f98385F6ba1adD4234e551ABba0f82Cb7C",
         "RootNetworkContract": "0xf408A064d640b620219F510963646Ed2bD5606BB",
     },
     "maya": {
